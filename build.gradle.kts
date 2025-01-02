791bb3ae34ee07a24e647d607cd0139abec49972
mkenna <mdjkenna5813@gmail.com>
2025-01-02 20:32:48 +0000

    Add Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("org.jetbrains.dokka") version "1.9.0"
     `maven-publish`
 }
 
@@ -36,6 +37,19 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 }
 
+// Task to generate sources jar
+tasks.register<Jar>("sourcesJar") {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
+// Task to generate Dokka Javadoc jar
+tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.named("dokkaJavadoc"))
+    from(tasks.named("dokkaJavadoc"))
+    archiveClassifier.set("javadoc")
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
@@ -43,6 +57,13 @@ publishing {
             groupId = "com.github.mdjkenna"
             artifactId = "GraphStateMachine"
             version = "0.0.0"
+
+            // Include the sources jar
+            artifact(tasks.named("sourcesJar"))
+
+            // Include the javadoc jar
+            artifact(tasks.named("dokkaJavadocJar"))
         }
     }
 }
+
